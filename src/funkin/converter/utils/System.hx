a6103562d3416ab10b07ae273cbe19aa942c71c7
CrusherNotDrip <crushernotdrip@tilnotdrip.org>
2025-12-01 21:00:40 -0500

    forgot that im leaving cross plat in mind

--- src/funkin/converter/utils/System.hx
+++ src/funkin/converter/utils/System.hx
@@ -8,6 +8,10 @@ class System
 {
 	public static function getSaveDirectory():String
 	{
+		#if sys
 		return CFFI.stringValue(NativeCFFI.lime_system_get_directory(1, null, 'FunkinConverter'));
+		#else
+		return null;
+		#end
 	}
 }
